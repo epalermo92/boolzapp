c2406f8ce6c303d2907366dd6f4f88801eca31cf
epalermo92 <emanuelepalermo1992@gmail.com>
2019-03-11 19:52:35 +0100

    aggiunta spunte di visualizzazione

--- script.js
+++ script.js
@@ -18,9 +18,11 @@ function automaticAnswers(){
   var time = document.createElement("small");
   var timejQ = $(time);
   var date = new Date();
+  var check = $("i.fas.fa-check-double");
 
+  check.css("color","blue");
   chat.append(divContMessRjQ.addClass("message-container"));
-  divContMessRjQ.append(messageReceivedjQ.addClass("message").addClass("received").text("Ok,va bene."));
+  divContMessRjQ.append(messageReceivedjQ.addClass("message").addClass("received").text("Ok"));
   messageReceivedjQ.append(timejQ.addClass("time").text(date.getHours()+":"+date.getMinutes()));
 }
 
@@ -37,11 +39,15 @@ function textEvent(e){
     var textToAdd = inputMessage.val();
     var time = document.createElement("small");
     var timejQ = $(time);
+    var check = document.createElement("i");
+    var checkjQ = $(check)
+    checkjQ.addClass("fas fa-check-double");
     var date = new Date();
 
     chat.append(divContjQ.addClass("message-container"));
     divContjQ.append(messageQ.addClass("message").addClass("sent").text(textToAdd));
     messageQ.append(timejQ.addClass("time").text(date.getHours()+":"+date.getMinutes()));
+    messageQ.append(checkjQ);
     inputMessage.val("");
     setTimeout(automaticAnswers,3000);
   }
